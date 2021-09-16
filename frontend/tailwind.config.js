4bf1cba9873c72024321b04e4fd8917a0389e7e9
Saisrujan18 <yayisaisrujan@gmail.com>
2021-09-16 17:41:44 +0530

    configured colors on tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white:"#F2F4F9",
+        lightBlue:"#ADC9FE",
+        black:"#070809",
+        darkBlue:"#4171E5",
+        grey:"#54575B",
+        darkOrange:"#F86E25",
+        orange:"#D59055"
+      }
+    },
     flex:{
       '1': '1 1 0%',
        auto: '1 1 auto',
