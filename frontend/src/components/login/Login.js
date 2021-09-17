522df34c71a4c867896f20bfe14e8a599b0b2d1d
PavanSriram <pavangud9@gmail.com>
2021-09-17 18:20:50 +0530

    clean up-1

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -6,6 +6,7 @@ import fire from "../login/assets/Camp.png";
 import bg from "../login/assets/BG.png"
 import { useAuth } from '../../authContext/AuthContext';
 import { useHistory } from 'react-router';
+import Spinner from '../Spinner';
 
 const styles = 
 {
@@ -23,9 +24,6 @@ const fonts = {
     header_font: "text-xl font-semibold"
 }
 
-
-
-
 export function Login() 
 {
     const [error, setError] = useState("");
@@ -40,10 +38,10 @@ export function Login()
             setLoading(true);
             await signInWithGoogle(); 
             console.log(auth.currentUser.email);
-            setLoading(false);
             history.push("/home");
         }
         catch(e){
+            setLoading(false);
             setError(e.message);
         }
     }
@@ -78,28 +76,3 @@ export function Login()
         </div>
     )
 }
-
-function Spinner() {
-    return (
-      <svg
-        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    );
-  }
