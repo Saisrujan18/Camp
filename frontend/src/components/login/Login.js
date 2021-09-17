9dba66b8d993d0c5df9f2133fe76fea0878acfb9
Manjunath0408 <nath.vasam@gmail.com>
2021-09-17 17:52:47 +0530

    spinner

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -14,7 +14,7 @@ const styles =
     login_body: "login-body rounded-l h-full",
     logo: "logo my-14 mx-auto flex items-center gap-x-2",
     login_header: "login-header my-5 mx-auto text-xl font-semibold",
-    login_button: "login-button my-2 mx-auto bg-blue-500 text-gray-50 px-10 py-2 login-btn",
+    login_button: "login-button flex items-center my-2 mx-auto bg-blue-500 text-gray-50 px-10 py-2 login-btn",
     login_image: "login-image hidden md:block md:flex-2 lg:flex-3 bg-whit  rounded-r-lg",
     logofont:"subpixel-antialiased font-bold text-2xl" 
 }
@@ -64,6 +64,7 @@ export function Login()
                     <div className={styles.login_header}>Login To Your Account</div>
                     
                     <button disabled = {loading} className={styles.login_button} onClick={handleLogin}>
+                        {loading && <Spinner />}
                         Log in with Google
                     </button>
                     {error && <div className="text-sm mx-auto text-red-600">{error}</div>}
@@ -77,3 +78,28 @@ export function Login()
         </div>
     )
 }
+
+function Spinner() {
+    return (
+      <svg
+        class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    );
+  }
