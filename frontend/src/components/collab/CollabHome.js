e64bd959c51ea53f5d54a16191aad73908818a29
Manjunath0408 <nath.vasam@gmail.com>
2021-09-29 18:52:28 +0530

    collab data fetch done. CollCards data visible.

--- frontend/src/components/collab/CollabHome.js
+++ frontend/src/components/collab/CollabHome.js
@@ -1,49 +1,64 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import Sidebar from '../Sidebar';
-import CollabCard from './CollabCard';
+import Sidebar from "../Sidebar";
+import CollabCard from "./CollabCard";
+import Spinner from "../Spinner";
 
-function CollabHome() 
-{
+function CollabHome() {
+  const [collabData, setCollabData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    // useEffect()
-    // {
-    //     axios.get("/collab",{})
-    //         .then(res=>{alert("fetched");})
-    //         .catch(err=>console.log(err))
-    // }
+  // get the data for collabCards.
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/collab")
+      .then((res) => {
+        setCollabData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  });
 
-    function addNewCollab(e)
-    {
-        e.preventDefault();
-        console.log("ADD");
-    }
+  function addNewCollab(e) {
+    e.preventDefault();
+    console.log("ADD");
+  }
 
-    return (
-        <div className="h-screen flex flex-row">
-            <Sidebar/>
-            <div className="flex-grow bg-white rounded-lg m-2 flex flex-col max-w-screen-lg">
-                
-                <div className="flex flex-row">
-                    <div className="m-2 ml-4 mb-4 text-3xl text-left font-bold">Collab</div>
-                    <div className="flex-grow"></div>
-                    <button className='place-self-end block p-2 m-1  hover:text-white hover:bg-blue-700 rounded font-bold' onClick={addNewCollab}>+ New Collab</button>
-                </div>
-                
-                <div className="grid grid-cols-2 md:grid-cols-3 place-content-stretch content-around bg-whit p-2 rounded-lg flex-grow">
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/>
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/>
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/>
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/>
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/> 
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/> 
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/> 
-                    <CollabCard title={"No"} description={"something"} author={"YOU"}/> 
-                </div>
-            </div>
-            {/* <Sidebar/> */}
+  return (
+    <div className="h-screen flex flex-row">
+      <Sidebar />
+      <div className="flex-grow bg-white rounded-lg m-2 flex flex-col max-w-screen-lg">
+        <div className="flex flex-row">
+          <div className="m-2 ml-4 mb-4 text-3xl text-left font-bold">
+            Collab
+          </div>
+          <div className="flex-grow"></div>
+          <button
+            className="place-self-end block p-2 m-1  hover:text-white hover:bg-blue-700 rounded font-bold"
+            onClick={addNewCollab}
+          >
+            + New Collab
+          </button>
         </div>
-    )
+        {/* Loading - show the spinner
+         * else - show the collabData
+         */}
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="grid grid-cols-2 md:grid-cols-3 place-content-stretch content-around bg-whit p-2 rounded-lg flex-grow items-center">
+            {collabData.map((collab) => (
+              <CollabCard
+                title={collab.title}
+                description={collab.description}
+                author={collab.author}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 export default CollabHome;
--- server.js
+++ server.js
@@ -12,42 +12,24 @@ app.use(express.json());
 const MONGO_DB_URI = process.env.MONGO_DB_URI;
 const PORT = process.env.PORT || 3001;
 
-const collabRouter = require ('./routers/collab');
+dbConnect().catch(err => console.log(err));
 
-app.use ('/home', collabRouter);
-
-
-const CollabModel=require("./models/collabModel");
-
-
-const arr = [{ title: 'Star Wars',	author: 'efw',description: "desc"},{ title: 'Sta Wars',author: 'ef',description: "des"}];
-
-
-main().catch(err => console.log(err));
-
-async function main() 
+async function dbConnect() 
 {
 	await mongoose.connect (
 		MONGO_DB_URI,
 		{
 			useNewUrlParser: true,
-			useCreateIndex: true,
 			useUnifiedTopology: true,
-			useFindAndModify: false,
 		}
-		// () => {
-		// 	CollabModel.insertMany(arr)
-		// 	.then(res=>console.log(res))
-		// 	.catch(err=>console.log(err))			
-		// 	console.log(MONGO_DB_URI);
-		// 	console.log ('connected to mongo_db');
-	  
-		// }
-
-		CollabModel.insertMany(arr)
-			.then(res=>console.log(res))
-			.catch(err=>console.log(err))
-	  );
-	  
+	  );	  
 }
-	app.listen (PORT, () => console.log (`server running on ${PORT}`));
+
+const collabRouter = require ('./routers/collab');
+app.use ('/collab', collabRouter);
+
+
+
+
+
+app.listen (PORT, () => console.log (`server running on ${PORT}`));
