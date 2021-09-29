1ec9584d4e22bb49fc83ba1555c3dc540d5eb075
PavanSriram <pavangud9@gmail.com>
2021-09-30 00:49:06 +0530

    in-page nav for Collab done. Color scheme pending

--- frontend/src/components/collab/Collab.js
+++ frontend/src/components/collab/Collab.js
@@ -7,11 +7,70 @@ export function Collab() {
 
     const [projName, setProjName] = useState("Project Name");
     const [owner, setOwner] = useState("Owner");
-    const [description, setDescription] = useState();
+    const [description, setDescription] = useState(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
+    Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
+    tortor at est feugiat fringilla non i sapien. Duis nunc purus, mattis vel porttitor eu,
+    dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
+    lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna i facilisis.
+    Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
+    dignissim.
+    Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
+    augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
+    aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
+    nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
+    blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
+    ultrices eget purus. Nam commodo tortor sit amet consequat aliquet. Lorem ipsum dolor sit amet, consectetur
+    adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
+    Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
+    tortor at est feugiat fringilla non  sapien. Duis nunc purus, mattis vel porttitor eu,
+    dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
+    lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna i facilisis.
+    Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
+    dignissim.
+    Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
+    augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
+    aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
+    nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
+    blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
+    ultrices eget purus. Nam commodo tortor sit amet consequat aliquet.
+    Lorem ipsum dolor sit amet, consectetur
+    adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
+    Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
+    tortor at est feugiat fringilla non sapien. Duis nunc purus, mattis vel porttitor eu,
+    dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
+    lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna facilisis.
+    Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
+    dignissim.
+    Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
+    augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
+    aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
+    nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
+    blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
+    ultrices eget purus. Nam commodo tortor sit amet consequat aliquet.`);
 
-    const scrollStyle = {
-        
-    };
+    const [links, setLinks] = useState(["Github", "LinkedIn"]);
+    const [members, setMembers] = useState(["Owner", "Person1", "Person2"]);
+    const [showInfo, setShowInfo] = useState(true);
+    const [showMembers, setShowMembers] = useState(false);
+    const [showChat, setShowChat] = useState(false);
+
+
+    const getMember = (member) => {
+        return (
+            <div className='bg-blue-400 rounded-lg p-4'>
+                <p className=''> {member} </p>
+            </div>
+        )
+    }
+
+    const getLinks = (link) => {
+        return (
+            <li className='bg-blue-400 rounded-lg p-4'> 
+                <a href='/'> {link} </a>
+            </li>
+        )
+    }
+
 
     return (
         // total screen
@@ -35,14 +94,14 @@ export function Collab() {
                             </div>
 
                             <div className='flex'>
-                                <Link to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
-                                transition duration-500 ease-in-out'> Info </Link>
+                                <button to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
+                                transition duration-500 ease-in-out' onClick={() => {setShowInfo(true); setShowChat(false); setShowMembers(false)}}> Info </button>
 
-                                <Link to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
-                                transition duration-500 ease-in-out'> Members </Link>
+                                <button to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
+                                transition duration-500 ease-in-out' onClick={() => {setShowInfo(false); setShowChat(false); setShowMembers(true)}}> Members </button>
 
-                                <Link to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
-                                transition duration-500 ease-in-out'> Chat </Link>
+                                <button to='/collab' className='rounded-lg text-md m-2 pl-4 pr-4 p-1 hover:bg-blue-500 
+                                transition duration-500 ease-in-out' onClick={() => {setShowInfo(false); setShowChat(true); setShowMembers(false)}}> Chat </button>
 
                                 <button className='rounded-lg text-md m-2 pl-4 pr-4 p-1 bg-blue-400 ml-auto hover:bg-blue-500 transition duration-500 ease-in-out'> Join </button>
                             </div>
@@ -55,59 +114,31 @@ export function Collab() {
                 {/* below of center */}
                 <main className="flex-1 overflow-y-auto p-2 bg-blue-200">
                     {/* <div className="relative"> */}
-                        <p className='bg-blue-400 rounded-lg text-1xl p-2 m-2'> Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
-                            Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
-                            tortor at est feugiat fringilla non in sapien. Duis nunc purus, mattis vel porttitor eu,
-                            dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
-                            lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna in facilisis.
-                            Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
-                            dignissim.
-                            <br />
-                            Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
-                            augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
-                            aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
-                            nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
-                            blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
-                            ultrices eget purus. Nam commodo tortor sit amet consequat aliquet. Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
-                            Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
-                            tortor at est feugiat fringilla non in sapien. Duis nunc purus, mattis vel porttitor eu,
-                            dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
-                            lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna in facilisis.
-                            Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
-                            dignissim.
-                            <br />
-                            Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
-                            augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
-                            aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
-                            nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
-                            blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
-                            ultrices eget purus. Nam commodo tortor sit amet consequat aliquet.
-                            Lorem ipsum dolor sit amet, consectetur
-                            adipiscing elit. Vestibulum non tellus lobortis felis tristique maximus quis sit amet odio.
-                            Pellentesque vulputate lectus vel sem imperdiet, ultrices tristique lorem venenatis. Sed a
-                            tortor at est feugiat fringilla non in sapien. Duis nunc purus, mattis vel porttitor eu,
-                            dignissim vitae nibh. Sed non metus volutpat, tempus nunc at, dignissim tortor. Maecenas
-                            lacinia erat eros, porta facilisis libero mattis ut. Cras tristique et urna in facilisis.
-                            Praesent placerat massa quis vulputate venenatis. Nulla sed ligula id nulla scelerisque
-                            dignissim.
-                            <br />
-                            Phasellus vestibulum et turpis ac rhoncus. Mauris semper, nisl fermentum faucibus ullamcorper,
-                            augue lacus dapibus orci, id consectetur mauris mauris nec turpis. Quisque sed enim blandit,
-                            aliquam purus eu, commodo sem. Nam accumsan vel mi ac vulputate. Morbi pellentesque diam nec
-                            nisl fringilla elementum. Nunc lacinia iaculis nibh at gravida. Nunc at elit quis orci dapibus
-                            blandit. Vestibulum lobortis varius lobortis. Vestibulum sapien augue, facilisis nec tempor ut,
-                            ultrices eget purus. Nam commodo tortor sit amet consequat aliquet.</p>
+                        <p className={'bg-blue-400 rounded-lg text-1xl p-2 m-2 ' + (showInfo ? "" : "hidden")}> {description} </p>
 
-                        <div>
-                            <ul className='m-2 space-y-1'>
-                                <li className='bg-blue-400 rounded-lg p-4'> <a href='#'> Github </a> </li>
-                                <li className='bg-blue-400 rounded-lg p-4'> <a href='#'> Linkdin </a> </li>
-                                <li className='bg-blue-400 rounded-lg p-4'> <a href='#'> Any other </a> </li>
-                            </ul>
-                        {/* </div> */}
-                    </div>
+                        {/* Members */}
+                        <div className={(showMembers ? "space-y-1 m-2" : "hidden")}>
+                            {
+                                members.map(member => (
+                                    getMember(member)
+                                ))
+                            }
+                        </div>
+
+                        {/* Chat */}
+                        <div className={(showChat ? "space-y-1 m-2" : "hidden")}>
+                            I dont know what chat includes
+                        </div>
+
+
+                        <div className='m-2 space-y-1'>
+                            <h1 className='m-2'> Links </h1>
+                            {
+                                links.map(link => (
+                                    getLinks(link)
+                                ))
+                            }
+                        </div>
                 </main>
             </div>
 
