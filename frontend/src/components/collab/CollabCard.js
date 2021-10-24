e933f0e0c38eccb77f6b979aaaa0d2193bbb618f
Manjunath0408 <nath.vasam@gmail.com>
2021-10-25 01:25:20 +0530

    some backend fixes

--- frontend/src/components/collab/CollabCard.js
+++ frontend/src/components/collab/CollabCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function CollabCard(props) {
-    const { title, author, description} = props;
+    const { title, author, description, _id, navigate} = props;
     return (
-        <div className="flex flex-col bg-white max-w-xs p-3 max-h-36 rounded-md shadow-md m-1">
+        <div className="flex flex-col bg-white max-w-xs p-3 max-h-36 rounded-md shadow-md m-1" onClick={() => navigate(_id)}>
           <div className="text-xl font-bold">{title}</div> 
           <div className="text-xs text-gray-500">{author}</div>
           <div className="text-sm my-1">{description}</div>
