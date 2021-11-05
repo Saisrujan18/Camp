ee728eb03a8c2f8f4a9ca40799aeda56ee20881e
PavanSriram <pavangud9@gmail.com>
2021-11-05 21:02:32 +0530

    minor changes-2

--- frontend/src/components/Edit.js
+++ frontend/src/components/Edit.js
@@ -8,21 +8,24 @@ function Edit() {
     const [btnText, setBtnText] = useState("Add");
     const [showEditor, setShowEditor] = useState(false);
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const [editorContent, setEditorContent] = useState();
 
     function onEditorStateChange(editorState) {
         setEditorState(editorState);
         console.log(convertToRaw(editorState.getCurrentContent()));
+        setEditorContent(convertToRaw(editorState.getCurrentContent()));
     }
+
 
     return (
         
-        <div>
-            <button className="place-self-end block p-2 m-1 hover:text-darkBlu hover:bg-gray-200 font-bold rounded"
+        <div className="flex flex-col place-items-end">
+            <button className=" place-self-auto bg-gray-200 block p-2 m-1 hover:text-darkBlu hover:bg-gray-200 font-bold rounded "
             onClick={() => {setShowEditor(!showEditor); (showEditor ? setBtnText("Add") : setBtnText("Close"));}}> {btnText} 
             </button>
 
             {showEditor ? 
-                <div>
+                <div className="flex flex-col place-items-end">
                     <Editor
                         editorState={editorState}
                         toolbarClassName="toolbarClassName"
@@ -31,7 +34,8 @@ function Edit() {
                         onEditorStateChange={onEditorStateChange}
                     />
                 
-                    <button className="place-self-end block p-2 m-1 hover:text-darkBlu hover:bg-gray-200 font-bold rounded"> Save </button>
+                    <button className="place-self-end block p-2 m-1 hover:text-darkBlu hover:bg-gray-200 font-bold rounded"
+                    > Save </button>
                 </div>
             : ""}
         </div>
