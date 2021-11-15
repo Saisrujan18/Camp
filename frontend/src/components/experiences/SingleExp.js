85381b39a5c94d61364c4a90792d2be0cfd09be7
PavanSriram <pavangud9@gmail.com>
2021-11-15 09:36:59 +0530

    still changing singleexp ui

--- frontend/src/components/experiences/SingleExp.js
+++ frontend/src/components/experiences/SingleExp.js
@@ -12,10 +12,16 @@ import "./Experiences.css";
 import Edit from "../Edit";
 import draftToHtml from 'draftjs-to-html';
 import Parser from 'html-react-parser'
-
+import img1 from "./images/1.jpg"
+import img2 from "./images/2.jpg"
+import img3 from "./images/3.jpg"
+import img4 from "./images/4.jpg"
+import img5 from "./images/5.jpeg"
+import img6 from "./images/6.jpg"
 
 export function SingleExp() {
   //  ALl the neccessary variables are declared over here.
+  let images = [img1, img2, img3, img4, img5, img6];
   const { user } = useAuth();
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
@@ -38,63 +44,78 @@ export function SingleExp() {
   // Renders the content of the whole screen.
   return (
     // total screen
-    <div className="h-screen flex ">
+    <div className="h-screen flex">
       {/* left sidebar */}
       <Sidebar />
 
       {/* Loads the spinner if its still loading or else rendering the content */}
 
-      <div className=" overflow-y-auto flex-1 py-3 bg-white my-2 mr-2 rounded-r-lg">
-        {/* above of center */}
-        {loading ? (
-          <Spinner />
-        ) : (
-          <div className="max-w-3xl mx-auto pt-1">
-            <div className="mx-3">
-              {/* The Whole Project Description */}
-              <div>
-                <div className="rounded-lg text-sm md:text-lg m-2 my-4">{expData.author}</div>
-                <div className="rounded-lg">
-                  <h1 className="text-3xl md:text-4xl text-justify font-bold my-14">
-                    {expData.title}
-                  </h1>
+      <div className="flex flex-col bg-white ">
+        <div className="w-full h-1/2">
+          <img 
+            src={images[Math.floor(Math.random()*images.length)]}
+            className="w-full rounded-lg p-2"
+          />
+        </div>
 
-                  {/* Inpage navigation */}
-                  <div className="flex">
-                    <button
-                      to="/collab"
-                      className={
-                        "text-md border-b-2 mt-2 pl-4 pr-4 p-1 " +
-                        (showInfo ? "border-darkBlu" : "")
-                      }
-                      onClick={() => {
-                        setShowInfo(true);
-                      }}
-                    >
-                      Info
-                      
-                    </button>
+        <div className=" overflow-y-auto flex-1 py-3 bg-whit my-2 mr-2 rounded-r-lg">
+          {/* above of center */}
+          {loading ? (
+            <Spinner />
+          ) : (
+            <div className="max-w-3xl mx-auto pt-1 bg-white h-full rounded-lg">
+              
+              <img 
+                src={images[Math.floor(Math.random()*images.length)]}
+                className="w-full h-1/2 rounded-lg p-0"
+              />
+              
+              <div className="mx-3 ">
+                {/* The Whole Project Description */}
+                <div>
+                  <div className="rounded-lg text-sm md:text-lg m-2 my-4">{expData.author}</div>
+                  <div className="rounded-lg">
+                    <h1 className="text-3xl md:text-4xl text-justify font-bold my-14">
+                      {expData.title}
+                    </h1>
+
+                    {/* Inpage navigation */}
+                    <div className="flex">
+                      <button
+                        to="/collab"
+                        className={
+                          "text-md border-b-2 mt-2 pl-4 pr-4 p-1 " +
+                          (showInfo ? "border-darkBlu" : "")
+                        }
+                        onClick={() => {
+                          setShowInfo(true);
+                        }}
+                      >
+                        Info
+                        
+                      </button>
+                    </div>
+                    <hr className="bg-whit h-0.5 border-none rounded-sm"></hr>
                   </div>
-                  <hr className="bg-whit h-0.5 border-none rounded-sm"></hr>
                 </div>
               </div>
-            </div>
 
-            {/* below of center */}
-            <main className="flex-1 p-2 bg-white ">
-              <h1 className={"font-bold m-1 " + (showInfo ? "" : " hidden")}>
-                {" "}
-                Description{" "}
-              </h1>
-              
-              <Edit 
-                sendTo = {"http://localhost:3001/api/experiences/id"}
-                id = {id}
-                expData = {expData}
-              />
-            </main>
-          </div>
-        )}
+              {/* below of center */}
+              <main className="flex-1 p-2 bg-white ">
+                <h1 className={"font-bold m-1 " + (showInfo ? "" : " hidden")}>
+                  {" "}
+                  Description{" "}
+                </h1>
+                
+                <Edit 
+                  sendTo = {"http://localhost:3001/api/experiences/id"}
+                  id = {id}
+                  expData = {expData}
+                />
+              </main>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
