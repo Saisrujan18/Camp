ce63c808de46995dc85192a8216d2a2350c06250
Harish Pradyot 7 <harishpradyot@gmail.com>
2021-11-26 20:08:02 +0530

    Nothing

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -11,13 +11,13 @@ import axios from 'axios';
 
 const styles = 
 {
-   body: "body flex flex-row rounded-lg text-blac bg-whit",
-    login_menu: "login-menu bg-whit rounded-l-lg flex-1",
-    login_body: "login-body rounded-l h-full",
+    body: "body flex flex-row rounded-lg text-blac",
+    login_menu: "login-menu bg-whit justify-around rounded-lg flex-1 small_l:rounded-l-lg",
+    login_body: "login-body flex flex-col rounded-l h-full",
     logo: "logo my-14 mx-auto flex items-center gap-x-2",
     login_header: "login-header my-5 mx-auto text-xl font-semibold",
-    login_button: "login-button flex items-center my-2 mx-auto bg-blue-500 text-gray-50 px-10 py-2 login-btn",
-    login_image: "login-image hidden md:block md:flex-2 lg:flex-3 bg-whit  rounded-r-lg",
+    login_button: "login-button flex items-center my-2 mx-auto bg-blue-500 rounded text-gray-50 px-10 py-2 transition duration-700 ease-in-out transform hover:scale-105 hover:shadow-glow_sb2",
+    login_image: "login-image items-center overflow-hidden h-auto hidden medium_l:block medium_l:w-3/4 large:w-5/7 rounded-r-lg",
     logofont:"subpixel-antialiased font-bold text-2xl" 
 }
 
@@ -52,30 +52,22 @@ export function Login()
     
     return (
         <div className={styles.body}>
-            
             <div className={styles.login_menu}>
-
                 <div className={styles.login_body}>
-
                     <div className={styles.logo}>
                         <img src={fire} className="w-9" alt="Not found"></img>
                         <div className={styles.logofont}>Camp</div>
                     </div>
-
-                    
-                    <div className={styles.login_header}>Login To Your Account</div>
-                    
+                    <div className={styles.login_header}>Login To Your Account</div>                    
                     <button disabled = {loading} className={styles.login_button} onClick={handleLogin}>
                         {loading && <Spinner />}
                         Log in with Google
                     </button>
                     {error && <div className="text-sm mx-auto text-red-600">{error}</div>}
                 </div>
-            
             </div>
-            
             <div className={styles.login_image}>
-                <img src={bg}  className="max-w-full max-h-full bg-contain imagee" alt="no"></img>
+                <img src={bg}  className="image w-full h-full bg-contain transition duration-500 ease-in-out transform hover:scale-105" alt="no"></img>
             </div>
         </div>
     )
