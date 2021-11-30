e615314f1ab1be48c8d7b4514c3a0a3555afb35c
Saisrujan18 <yayisaisrujan@gmail.com>
2021-12-01 04:42:16 +0530

    Home almost done.

--- frontend/src/components/home/AltHome.js
+++ frontend/src/components/home/AltHome.js
@@ -9,6 +9,21 @@ import fire from "../home/assets/Camp.png";
 // This page shows the links to all the pages of the webapp
 // User can navigate through the webapp from the home
 
+
+const styles = {
+    body: "body flex flex-row rounded-lg text-blac",
+    login_menu:
+      "login-menu bg-whit justify-around rounded-lg flex-1 small_l:rounded-l-lg",
+    login_body: "login-body flex flex-col rounded-l h-full",
+    logo: "logo  mx-auto flex items-center gap-x-2 text-black",
+    login_header: "login-header my-5 mx-auto text-xl font-semibold",
+    login_button:
+      "login-button flex items-center my-2 mx-auto bg-blue-500 rounded text-gray-50 px-10 py-2 transition duration-700 ease-in-out transform hover:scale-105 hover:shadow-glow_sb2",
+    login_image:
+      "login-image items-center overflow-hidden h-auto hidden medium_l:block medium_l:w-3/4 large:w-5/7 rounded-r-lg",
+    logofont: "subpixel-antialiased font-bold text-3xl text-black",
+  };
+
 export function AltHome() 
 {
     const [tiles, setTiles] =  useState([
@@ -37,19 +52,19 @@ export function AltHome()
     }
     const getTitle = () => {
         return (
-            <div className="flex flex-row justify-center h-full">
-                <img src={fire} className="w-auto h-2/3 ml-0 self-center" alt="Not found"></img>
-                <div className="flex flex-col justify-center">
-                    <div className="text-whit subpixel-antialiased font-bold text-lg medium_l:text-xl px-4 self-center">Welcome to </div>
-                    <div className="font-camp text-whit subpixel-antialiased font-italic text-3xl medium_l:text-6xl px-4 self-center">Camp</div>
-                </div>
+            <div className="flex flex-row justify-center py-4 border-b-2 border-grey-400 bg-whit rounded-t-lg">
+                    <div className={styles.logo}>
+                        <div className={styles.logofont}>Welcome to</div>
+                        <img src={fire} className="w-12" alt="Not found"></img>
+                        <div className={styles.logofont}>Camp</div>
+                    </div>
             </div>
         )
     }
     const getContent = () => {
         return ( 
-            <div className="main h-full w-full overflow-y-auto">    
-                <div className="grid medium_l:grid-cols-2 grid-rows-auto grid-cols-1 place-content-center place-items-center gap-x-8 gap-y-4 p-4 small_l:p-6">
+            <div className="main h-full w-full overflow-y-auto bod">    
+                <div className="grid medium_l:grid-cols-2 grid-rows-auto grid-cols-1 place-content-center place-items-center gap-x-8 gap-y-4 p-4 small_l:p-6 ">
                     {
                         tiles.map((tile, index)=>(
                             getTile(tile, index)
@@ -61,12 +76,12 @@ export function AltHome()
     }
     const getHomeScreen = () => {
         return (
-            <div className="BG bg-fixed w-screen p-2">
+            <div className="BG bg-fixed w-screen p-1">
                 <div className="flex flex-col justify-center min-h-screen h-full py-4 small:mx-4 medium:mx-4 large:mx-4">
-                    <div className="bg-blue-400 pattern-bg p-5 h-home-title-small medium_l:h-home-title rounded-t-lg w-auto">
+                    <div className="rounded-t-lg w-auto">
                         {getTitle()}
                     </div>
-                    <div className="flex-grow bg-whit85 p-5 rounded-b-lg h-auto w-auto">
+                    <div className="flex-grow bg-whit85 p-5 rounded-b-lg h-auto w-auto ">
                         {getContent()}
                     </div>
                 </div>
