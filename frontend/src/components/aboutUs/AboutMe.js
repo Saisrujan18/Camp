a5ef9ae18f179caef768388a5039a15f71ac0fc2
PavanSriram <pavangud9@gmail.com>
2021-12-01 21:15:39 +0530

    About me

--- frontend/src/components/aboutUs/AboutMe.js
+++ frontend/src/components/aboutUs/AboutMe.js
@@ -2,10 +2,10 @@ function AboutMe({url, description})
 {
     // Variables required for the popup are declared over here.
     return (
-        <div className="flex flex-col w-1/4 rounded-lg mx-2 my-2 bg-white shadow-lg p-4 justify-between gap-10 h-full inset-y-0 flex-1">
-            <img src={url} alt="Profile photo"></img>
-            <div className="w-auto ">
-                {description}
+        <div className="flex flex-col rounded-lg mx-2 my-2 bg-white shadow-lg p-4 gap-10 h-full inset-y-0 ">
+            <img src={url} alt="Profile photo" className="w-52 small:w-72 medium:w-full place-self-center rounded-lg"></img>
+            <div className="flex flex-col w-auto overflow-visible">
+                <p>{description}</p>
             </div>
         </div>
     );
--- frontend/src/components/aboutUs/AboutUs.js
+++ frontend/src/components/aboutUs/AboutUs.js
@@ -2,12 +2,11 @@ import { useState } from "react";
 import { TextField } from "@mui/material";
 import { yellow } from "@mui/material/colors";
 import AboutMe from "./AboutMe.js"
-import DP from "./images/deadpool.jpg"
 import pavan from "./images/pavan.jpg"
 import harish from "./images/harish.jpg"
 import manjunath from "./images/manjunathr.jpg"
 import srujan from "./images/srujanr.jpg"
-// import Sidebar from "../Sidebar.js";
+import { Link } from "react-router-dom";
 
 function AboutUs() 
 {
@@ -18,40 +17,56 @@ function AboutUs()
     const url3 = manjunath;
     const url4 = srujan;
 
+    const HomeButton = () => {
+      return(
+        <Link to="/home"
+            className="bg-lightBlu30 py-3 px-4 m-2 w-30 rounded flex flex-row gap-x-4 place-self-start 
+            font-semibold text-darkBlu shadow-glow_sb1 transition duration-500 ease-in-out transform hover:bg-lightBlu20 hover:scale-105 hover:shadow-glow_sb2">
+    
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+            Home
+        </Link>
+      )
+    }
+
     return (
-        <div className="flex flex-col rounded-lg mx-2 my-2 bg-whit justify-between overflow-y-auto">
-            <div className="flex-grow"></div>
-              <div className="m-2 ml-4 mb-4 text-3xl text-left font-medium flex flex-row gap-x-2 place-content-center">
+        <div className="flex flex-col h-screen rounded-lg mx-2 my-2 bg-whit overflow-y-auto">
+            <div className="flex flex-row w-full mx-auto sm:justify-between ">
+                
+              <HomeButton />
+              
+              <div className=" justify-between m-2 ml-4 mb-4 text-3xl text-left font-medium flex flex-row gap-x-2 place-self-center">
+                
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 self-center" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                    </svg>
+                  <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                </svg>
                 About Us
-              </div>
-              {/* <button
+                </div>
+                <div className=""></div>
+            </div>
 
-				>
-					<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-            	</button> */}
-              <div className="flex-grow"></div>
-              <div className="flex-grow bg-white h-0.5 rounded-br-lg"></div>
+            
+            <div className="bg-white h-0.5 rounded-br-lg"></div>
 
-            <div className=" flex flex-row rounded-lg mx-2 my-2 bg-whit justify-between overflow-y-auto">
+            <div className=" grid grid-cols-1 small:grid-cols-2 medium:grid-cols-4 rounded-lg mx-2 my-2 bg-whit justify-between overflow-y-auto flex-grow small:flex-col gap-y-2">
                 <AboutMe url={url1} description={"FrontEnd Developer"}/>
                 <AboutMe url={url2} description={"FrontEnd Developer"}/>
                 <AboutMe url={url3} description={"FullStack Developer"}/>
                 <AboutMe url={url4} description={"FullStack Developer"}/>
             </div>
+
+            
         </div>
     );
 }
